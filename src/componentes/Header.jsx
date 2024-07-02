01dd57f046d4a44f79b4c19191987a1921cff103
Nicolas Basualdo <basualdocarlos691@gmail.com>
2024-07-02 09:43:17 -0300

    Primer consumo de api

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -5,10 +5,10 @@ import PokeIco from '../assets/Pokemon.png';
 
 
 export default function Header() {
-  return (
-    <div className='navbar lg-body-tertiary' >
-            <img className='PokeLogo' src={PokeIco} alt="LogoPokemon" />
-            <input className="form-control me-2" type="search" placeholder="Busca Tu Pokemon" aria-label="Search"></input>
-    </div>
-  )
+    return (
+        <div className='navbar lg-body-tertiary' >
+                <img className='PokeLogo' src={PokeIco} alt="LogoPokemon" />
+                <input className="form-control me-2" type="search" placeholder="Busca Tu Pokemon" aria-label="Search"></input>
+        </div>
+    )
 }
--- src/componentes/Home.jsx
+++ src/componentes/Home.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import '../estilos/home.css';
 import Header from './Header';
+import axios from 'axios';
+import {URL_POKEMON} from '../api/apiRest'
+
+
+
+
 
 export default function Home() {
+
+  useEffect(() =>{
+
+    const api = async () =>{
+      const apiPoke = await axios.get(`${URL_POKEMON}`)
+      console.log(apiPoke.data);
+    } 
+
+      api()
+
+
+
+  }, [])
+
+
+
+
+
+
   return (
     <Header/>
   )

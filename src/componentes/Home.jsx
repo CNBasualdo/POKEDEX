471dcb70b69f78930d33c1a0816b7380bec1a76d
Nicolas Basualdo <basualdocarlos691@gmail.com>
2024-07-11 10:25:35 -0300

    punto de recuperacion, modificar limit

--- src/componentes/Home.jsx
+++ src/componentes/Home.jsx
@@ -11,7 +11,8 @@ export default function Home() {
 
   useEffect (() => {
     const api  = async () => {
-      const apiPoke = await axios.get(`${URL_POKEMON}`);
+      const limit =15
+      const apiPoke = await axios.get(`${URL_POKEMON}/?offset=0&limit=${limit}`);
       setArrayPokemon(apiPoke.data.results);
     };
     api ()
@@ -25,9 +26,25 @@ export default function Home() {
     <div>
       <Header/>
 
-
-
-
+      <section>
+          <ul className="pagination">
+              <li className="page-item disabled">
+                <a className="page-link">Previous</a>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="#">1</a>
+              </li>
+              <li className="page-item active" aria-current="page">
+                <a className="page-link" href="#">2</a>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="#">3</a>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="#">Next</a>
+                </li>
+          </ul>
+      </section>
       
   
       <div className='Tarjetero'>

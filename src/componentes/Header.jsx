e7bc3d04be0f70c768d687a1b5467130c7a0e5da
Nicolas Basualdo <basualdocarlos691@gmail.com>
2024-07-16 16:26:32 -0300

    Barra de busqueda

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -4,11 +4,14 @@ import PokeIco from '../assets/Pokemon.png';
 
 
 
-export default function Header() {
+export default function Header({obetenerBusqueda}) {
+
     return (
         <div className='navbar lg-body-tertiary' >
                 <img className='PokeLogo' src={PokeIco} alt="LogoPokemon" />
-                <input className="form-control me-2" type="search" placeholder="Busca Tu Pokemon" aria-label="Search"></input>
+                <input className="form-control me-2" type="search" placeholder="Busca Tu Pokemon" aria-label="Search"
+                onChange={(e => obetenerBusqueda(e.target.value))}
+                ></input>
         </div>
     )
 }
--- src/componentes/Home.jsx
+++ src/componentes/Home.jsx
@@ -8,47 +8,52 @@ import Card from './Card';
 
 export default function Home() {
   const [arrayPokemon, setArrayPokemon] = useState([]);
+  const [globalpokemon, setGlobalPokemon] = useState([])
+  const [xpage, setXpage] = useState(1);
+  const [buscar, setBuscar] = useState('');
 
   useEffect (() => {
     const api  = async () => {
-      const limit =15
-      const apiPoke = await axios.get(`${URL_POKEMON}/?offset=0&limit=${limit}`);
+      const limit =151
+      const xp = (xpage - 1) * limit
+      const apiPoke = await axios.get(`${URL_POKEMON}/?offset=${xp}&limit=${limit}`);
       setArrayPokemon(apiPoke.data.results);
     };
     api ()
-  }, []);   
+    getGlobalPokemons()
+  }, [xpage]);   
+
+  const getGlobalPokemons = async ()=>{
+    const res = await axios.get(`${URL_POKEMON}?offset=0&limit=151`)
+
+    const promises = res.data.results.map(pokemon => {
+      return pokemon; 
+    });
+    const results = await Promise.all(promises)
+    setGlobalPokemon(results)
+  };
+
+  const filterPokemons = buscar?.length > 0 
+  ? globalpokemon ?.filter(pokemon => pokemon?.name?.includes(buscar))
+  : arrayPokemon; 
   
-  
+  const obetenerBusqueda = (e) =>{
+    const texto = e.toLowerCase()
+    setBuscar(texto)
+    setXpage(1)
+  }
   
 
 
   return (
     <div>
-      <Header/>
+      <Header obetenerBusqueda={obetenerBusqueda} />
 
-      <section>
-          <ul className="pagination">
-              <li className="page-item disabled">
-                <a className="page-link">Previous</a>
-              </li>
-              <li className="page-item">
-                <a className="page-link" href="#">1</a>
-              </li>
-              <li className="page-item active" aria-current="page">
-                <a className="page-link" href="#">2</a>
-              </li>
-              <li className="page-item">
-                <a className="page-link" href="#">3</a>
-              </li>
-              <li className="page-item">
-                <a className="page-link" href="#">Next</a>
-                </li>
-          </ul>
-      </section>
+    
       
   
       <div className='Tarjetero'>
-        {arrayPokemon.map((card, index) =>{
+        {filterPokemons.map((card, index) =>{
           return <Card  key={index} card={card} />
         })}
     </div>

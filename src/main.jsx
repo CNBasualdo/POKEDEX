08e587f308913ce64400b1b6aab2a9b948f69644
Nicolas Basualdo <basualdocarlos691@gmail.com>
2024-07-16 16:43:04 -0300

    Se instalo React-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {createHashRouter, RouterProvider} from 'react-router-dom'
 import './estilos/index.css';
-import App from './App.jsx'
+import App from './App.jsx';
+import Home from './componentes/Home.jsx'
+
+const router = createHashRouter([
+  {
+    path:'/',
+    element: <Home/>
+  }
+])
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
